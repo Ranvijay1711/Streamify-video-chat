dbcedc5083e4508dbefce497639f932b59a12721
Ranvijay1711 <ranvijay255217@gmail.com>
2025-08-03 11:00:05 +0530

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,9 +5,16 @@ export const signup=async(req,res)=>{
     if(!email ||!password || !fullName){
       return res.status(400).json({message:"All frilds are required"})
     }
-  }
+  
   if(password.lenght<6){
-    
+    return res.status(400).json({message:"Password must be 6 characters"})
+  }
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if(!emailRegex.test(email)){
+    return res.status(400).json({message:"Please Provide a correct email"});
+   }
+  }catch(error){
+
   }
 }
 export const login=async(req,res)=>{
