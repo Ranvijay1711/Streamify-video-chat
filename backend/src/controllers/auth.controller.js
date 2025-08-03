4384fef53e6ff85d5b0d2985bf25b74c8210e436
Ranvijay1711 <ranvijay255217@gmail.com>
2025-08-03 09:28:29 +0530

    start

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,14 @@
 export const signup=async(req,res)=>{
-  res.send('Signup Route');
+  const {email,password,fullName}=req.body;
+
+  try{
+    if(!email ||!password || !fullName){
+      return res.status(400).json({message:"All frilds are required"})
+    }
+  }
+  if(password.lenght<6){
+    
+  }
 }
 export const login=async(req,res)=>{
   res.send('login');
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import authRoutes from './routes/auth.route.js'
 const port=process.env.PORT;
 const app=express();
+app.use(express.json());
 app.use("/api/auth",authRoutes);
 
 app.listen(port,()=>{
